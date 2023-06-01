916b47957569c8c3ce29c67ca182b5d86f50ff30
Paweł Łangowski <langowski.pawel183@gmail.com>
2023-06-01 13:46:19 +0200

    Dodanie roszady

--- src/main/java/com/pawlan/Main.java
+++ src/main/java/com/pawlan/Main.java
@@ -1,12 +1,11 @@
 package com.pawlan;
 
-import com.pawlan.gui.BoardGUI;
 import com.pawlan.gui.MainWindow;
 
 public class Main {
     public static void main(String[] args) {
-        //MainWindow mainWindow = new MainWindow();
+        new MainWindow();
 
-        BoardGUI boardGUI = new BoardGUI("test", "test", 10.0);
+        //BoardGUI boardGUI = new BoardGUI("test", "test", 10.0);
     }
 }
